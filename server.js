b85b0cc42af4199407a8732471bffaeb2dae17cc
Yorvis <yorvis.melendez@gmail.com>
2023-09-30 17:04:05 -0500

    Route importation

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const routes = require('./src/routes/elasticRoutes')
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

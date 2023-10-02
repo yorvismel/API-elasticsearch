8790ec091c49145582549b0895cc13bc1b33e2aa
Yorvis <yorvis.melendez@gmail.com>
2023-10-02 16:24:30 -0500

    Update code formatting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const routes = require('./src/routes/elasticRoutes')
+const routes = require("./src/routes/elasticRoutes");
 
 const app = express();
 const port = 3000;

eec11f192f7665bf126a7c63e0036e577f446871
Yorvis <yorvis.melendez@gmail.com>
2023-10-02 16:30:23 -0500

    Added a new route for retrieving all documents and performed refactoring.

--- src/routes/elasticRoutes.js
+++ src/routes/elasticRoutes.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express();
 const {
   addDocuments,
-  updateResource,
-  getDocument,
-  searchDocuments,
+  getDocumentById,
+  getAllDocumentsInIndex,
+  searchDocumentsFilters,
+  updateDocuments,
 } = require("../controllers/elasticControllers");
 
 router.post("/:indexName/documents", addDocuments);
-router.put("/:indexName/documents/:id", updateResource);
-router.get("/:indexName/documents/:id", getDocument);
-router.get("/:indexName/search", searchDocuments);
+router.put("/:indexName/documents/:id", updateDocuments);
+router.get("/:indexName/documents/:id", getDocumentById);
+router.get("/:indexName/search", searchDocumentsFilters);
+router.get("/:indexName/all_documents", getAllDocumentsInIndex);
 
 module.exports = router;

255530195ce9e3b5a745fc1d4b58bd064cffa470
Yorvis <yorvis.melendez@gmail.com>
2023-10-02 16:27:25 -0500

    Refactoring, formatting, and addition of the controller to retrieve all documents

--- src/controllers/elasticControllers.js
+++ src/controllers/elasticControllers.js
@@ -5,15 +5,27 @@ const addDocuments = async (req, res, next) => {
     const { indexName } = req.params;
     const { documents } = req.body;
 
+    // Ensure 'indexName' and 'documents' are provided
+    if (!indexName || !documents) {
+      return res
+        .status(400)
+        .json({ message: "Both 'indexName' and 'documents' are required." });
+    }
+
+    // Prepare the request body for bulk indexing
     const body = documents.flatMap((doc) => [
       { index: { _index: indexName } },
       doc,
     ]);
 
+    // Execute the bulk indexing operation
     const { body: bulkResponse } = await client.bulk({ refresh: true, body });
 
+    // Check for errors in the bulk response
     if (bulkResponse && bulkResponse.errors) {
       const erroredDocuments = [];
+
+      // Extract information about errored documents
       bulkResponse.items.forEach((action, i) => {
         const operation = Object.keys(action)[0];
 
@@ -25,19 +37,22 @@ const addDocuments = async (req, res, next) => {
         }
       });
 
-      res.status(400).json({
+      // Return an error response with details of failed documents
+      return res.status(400).json({
         message: "Some documents failed to be indexed.",
         errors: erroredDocuments,
       });
-    } else {
-      res.status(201).json({ message: "Documents added successfully", body });
     }
+
+    // Return a success response when all documents are indexed
+    res.status(201).json({ message: "Documents added successfully", body });
   } catch (error) {
     console.error("Error in addDocuments:", error);
     next(error);
   }
 };
-const updateResource = async (req, res, next) => {
+
+const updateDocuments = async (req, res, next) => {
   try {
     const { indexName, id } = req.params;
     const document = req.body;
@@ -46,11 +61,8 @@ const updateResource = async (req, res, next) => {
       index: indexName,
       id,
       body: document,
-
       refresh: true,
     });
-
-    
 
     res.status(201).json({ message: "Document updated successfully", body });
   } catch (error) {
@@ -59,7 +71,7 @@ const updateResource = async (req, res, next) => {
   }
 };
 
-const getDocument = async (req, res, next) => {
+const getDocumentById = async (req, res, next) => {
   try {
     const { indexName, id } = req.params;
 
@@ -79,61 +91,115 @@ const getDocument = async (req, res, next) => {
   }
 };
 
-const searchDocuments = async (req, res, next) => {
+const searchDocumentsFilters = async (req, res, next) => {
   const { indexName } = req.params;
   const { q, filters } = req.query;
-
   try {
-    const filtersArray = filters.split(",").map((filter) => {
-      const [field, value] = filter.split(":");
-      console.log("field, value:", field, value);
-      return {
-        match: {
-          [field]: value,
-        },
-      };
-    });
-    
-    const query = {
-      bool: {
-         must: [
-          {
-            query_string: {
-              query: q,
-              default_operator: "AND",
-            },
-          },
-          ...filtersArray,
-        ],
-      },
-    };
+    const filtersArray = filters.split(",").map(parseFilter);
 
-    const response = await client.search({
-      index: indexName,
-      body: { query },
-    });
-
-    if (response.body && response.body.hits) {
-      const hits = response.body.hits;
-
-      if (hits.total.value > 0) {
-        // If there are results, send a successful response with the results.
-        res.json({
-          success: true,
-          message: "Success search",
-          results: hits.hits,
-        });
-      } else {
-        // If there are no results, send a success message with no results.
-        res.json({ success: true, message: "Success search", response });
-      }
-    } else {
-      // If there is no 'hits' property in the response, send a success message with no results.
-      res.json({ success: true, message: "Success search", response });
+    if (!isValidFilters(filtersArray)) {
+      return res.status(400).json({ error: "Invalid filters" });
     }
+    const query = buildQuery(q, filtersArray);
+    const response = await client.search({ index: indexName, body: { query } });
+
+    if (!response.body || !response.body.hits) {
+      return res.json({ success: true, message: "Success search", response });
+    }
+
+    const hits = response.body.hits;
+    const results = hits.total.value > 0 ? hits.hits : [];
+
+    return res.json({ success: true, message: "Success search", results });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Error search" });
+    return res.status(500).json({ error: "Error search" });
   }
 };
-module.exports = { getDocument, addDocuments, updateResource, searchDocuments };
+
+const parseFilter = (filter) => {
+  const [field, value] = filter.split(":");
+  return { match: { [field]: value } };
+};
+
+const isValidFilters = (filtersArray) => {
+  if (!Array.isArray(filtersArray)) {
+    return false;
+  }
+
+  for (const filter of filtersArray) {
+    if (!filter.match || typeof filter.match !== "object") {
+      return false;
+    }
+    const matchKeys = Object.keys(filter.match);
+
+    if (matchKeys.length !== 1) {
+      return false;
+    }
+
+    const field = matchKeys[0];
+    const value = filter.match[field];
+
+    if (typeof field !== "string" || typeof value !== "string") {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const buildQuery = (q, filtersArray) => {
+  return {
+    bool: {
+      must: [
+        {
+          query_string: {
+            query: q,
+            default_operator: "AND",
+          },
+        },
+        ...filtersArray,
+      ],
+    },
+  };
+};
+
+const getAllDocumentsInIndex = async (req, res) => {
+  const { indexName } = req.params;
+
+  try {
+    let response = await client.search({
+      index: indexName,
+      size: 100, // Adjust the maximum size of results according to your needs.
+      scroll: "1m", // Set a 'scroll' time to paginate the results.
+      body: {
+        query: {
+          match_all: {}, // Retrieve all documents.
+        },
+      },
+    });
+
+    if (
+      !response.body ||
+      !response.body.hits ||
+      response.body.hits.hits.length === 0
+    ) {
+      return res
+        .status(202)
+        .json({ message: "All documents index :", indexName, response });
+    }
+  } catch (error) {
+    console.error("Error when retrieving documents from Elasticsearch:", error);
+    res
+      .status(500)
+      .json({ error: "Error when retrieving documents from Elasticsearch" });
+  }
+};
+
+module.exports = {
+  getDocumentById,
+  addDocuments,
+  updateDocuments,
+  searchDocumentsFilters,
+  getAllDocumentsInIndex,
+};
